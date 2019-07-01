71eeb160ea1f61793a5914bb20ea6ea66654bc98
LeO So <leoso3037@gmail.com>
2019-07-02 01:24:49 +0800

    add else part

--- driving.py
+++ driving.py
@@ -3,8 +3,13 @@ age = input('How old are you?')
 age = int(age)
 if country == 'Taiwan':
 	if age >= 18:
-		print('You can drive ~~~~~')
+		print('You can drive ~~~~~' )
+	else:
+		print('You can not QQ')
+elif country == 'USA':	
+	if age >= 16:
+		print('You can drive ~~~~~' )
 	else:
 		print('You can not QQ')
 else:
-	print('I don\'t know~ sorry')
+	print('You can only search for Taiwan or USA')
